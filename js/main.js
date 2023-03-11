b74d79a10d7454904c191e1a39481d260549f15a
agustinpietrafesa <ahpietrafesa@gmail.com>
2023-03-11 18:34:56 -0300

    intento de guardar datos socios

--- js/main.js
+++ js/main.js
@@ -12,6 +12,10 @@ const welcome = () => {
     location.href = "inicio.html"
 }
 
+const verResultados = () => {
+    location.href = "misPesajes.html"
+}
+
 document.addEventListener('DOMContentLoaded', () => {
    if(JSON.parse(localStorage.getItem('socios')) != null){
     socios = JSON.parse(localStorage.getItem('socios'));
@@ -33,6 +37,7 @@ const crearSocio = (evt) => {
     const socio = {
         Nombre: nombre,
         Apellido: apellido,
+        Usuario: usuario,
         Email: email,
         Password: password
     }
@@ -59,13 +64,60 @@ const inicioSesion = (evt) => {
     let emailIngresado = document.querySelector('#emailSesion').value
     let passwordIngresada = document.querySelector('#passwordSesion').value
 
-    welcome()
-    formInicioSesion.reset()
 
- }
+    let usuario = socios.find(socio => socio.Email === emailIngresado)
+
+
+    if(usuario) {
+       if(usuario.Password === passwordIngresada){
+        welcome()
+        formInicioSesion.reset()
+       }else{
+        alert("mal")
+       }
+       }else{
+        alert("mal")
+       }
+    }
+
+ 
 
 if(formInicioSesion){
     formInicioSesion.addEventListener('submit', inicioSesion);
 }
 
+
+/***************Ingreso de datos  ********************/
+
+
+const formMisDatos = document.querySelector('#formMisDatos')
+
+
+const ingresoDatos = (evt) => {
+    evt.preventDefault()
+
+    let nombre = document.querySelector('#nombre').value
+    let apellido = document.querySelector('#apellido').value
+    let usuario = nombre + " " + apellido
+    let edad = document.querySelector('#edad').value
+    let peso = document.querySelector('#peso').value
+    let altura = document.querySelector('#altura').value
+    let genero = document.querySelector('#genero').value
+
+    const sociosDatos = {
+        Edad: edad,
+        Peso: peso,
+        Altura: altura,
+        Genero: genero
+    }
+
+
+        
+}
+
+if(formMisDatos){
+    formMisDatos.addEventListener('submit', ingresoDatos);
+}
+
+/**************Pesajes y calculos****************/
 
